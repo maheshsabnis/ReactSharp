48ae30573fef42b58e8a6c39015215afdc28b78e
Mahesh Sabnis <sabnis_m@hotmail.com>
2020-10-30 17:24:45 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,29 +4,46 @@ import './index.css';
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './../node_modules/d3/dist/d3';
 
-// import createStore
-import {createStore} from 'redux';
+// import createStore, applyMiddleware, compose
+// applyMiddleware: Used to register SAGA middleware for store,
+// applyMiddleware will manage all async calls
+// compose: Used to register the Redux Simulator with application
+import {createStore, applyMiddleware, compose} from 'redux';
 // import Provider
 import {Provider} from 'react-redux'
 // import reducer
-import rootReducer from './reduxappfunctionalcomponents/reducers/redecurs'; 
-// import MainComponent
-import MainComponent from './reduxappfunctionalcomponents/MainComponent';
-// import the BrowserRouter for routing Execution 
+import reducer from './sagaapps/reducers/reducers';
 
+// import the createSagaMiddleware to confugure SAGA middleware
+// with the applciation
+import createSagaMiddleware from 'redux-saga'; 
+
+// import MainComponent
+import MainSagaComonent from './sagaapps/MainSagaComponent';
 import * as serviceWorker from './serviceWorker';
- 
-// create a store using the reducer
-// also add a window object that will simulate the redux in browser (only in dev.)
-let store = createStore(rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__());
+import rootSaga from './sagaapps/sagas';
+// create an instance of Saga Middleware
+const appSagaMiddleware = createSagaMiddleware();
+
+
+// create a store with Simulator Compose
+const parameterEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// create a store using reducer, then confugure the Redux Simulator and
+// the SAGA middleware so that, all async operations will be monitored
+let store = createStore(
+ reducer,
+ parameterEnhancer(applyMiddleware(appSagaMiddleware))
+);
+
+// keep running the saga middleware 
+appSagaMiddleware.run(rootSaga);
 
 
  // the Provider will create a subscription of 'store'
  // for all components executing under it
 ReactDOM.render(
   <Provider store={store}>
-    <MainComponent></MainComponent>
+    <MainSagaComonent></MainSagaComonent>
   </Provider>
    ,
   document.getElementById('root')
